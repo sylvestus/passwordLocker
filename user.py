aa31073682ab9c5a0ac4c618e49889e87e9e23ef
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-21 07:39:18 +0300

    add user class

---
+++ user.py
@@ -0,0 +1,19 @@
+
+import string
+import random
+
+
+class User:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+    userList = []
+    
+    
+
+       
+
+
+
+        
