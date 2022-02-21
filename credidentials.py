34af152b3a4b4cff94a6bd67036f24d96e8e2fee
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-21 07:49:08 +0300

    add credidential class

---
+++ credidentials.py
@@ -0,0 +1,11 @@
+from user import User
+
+
+class Credidential:
+    def __init__(self,accountType,username,password):
+        self.accountType = accountType
+        self.username = username
+        self.password = password
+    
+    credList = []
+    
